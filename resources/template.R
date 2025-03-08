4e5d1faf7dc5098463966b7f1da2edda0a0d36a0
Matt Ashby <mpjashby@gmail.com>
2025-03-08 17:12:07 +0000

    Add Chapters 13 and 14 for 2025

---
+++ resources/template.R
@@ -0,0 +1,46 @@
+# Replace this text with a short explanation of what this script does, e.g.
+# 'This script produces a map of residential burglary in Lagos, Nigeria, in
+# 2024'
+
+# REMEMBER (you can delete this part of the code once you have read it)
+# * Make sure your code follows all aspects of the Tidyverse style guide -- you
+#   can use the styler package to help you with this.
+# * Write a comment for each line of your code to explain what that code does
+#   -- this will be useful to you in future if you need to come back to this
+#   code later.
+# * Write any temporary code (code that is needed to develop this script, but is
+#   not needed for the script itself) in the R Console, not in this script.
+# * Check your code by running `learncrimemapping::check_code()` and fixing any
+#   issues that are reported **before you submit your script**.
+
+
+
+# PREPARE ----------------------------------------------------------------------
+
+
+## Load packages ----
+# Use pacman to load *all* the packages you need for this script here. Do not
+# load packages anywhere else in the script.
+
+
+## Load and clean data ----
+# Load all the data you will need here. If the data cannot be downloaded
+# directly from a URL, make sure include the code needed to download the file to
+# a local file first, then load the data from there. In this section you should
+# also do any necessary initial cleaning of the data, e.g. to clean the column
+# names.
+
+
+
+# WRANGLE ----------------------------------------------------------------------
+# Do any other wrangling of the data that is required before you create one or
+# more maps. If several different types of data wrangling are required, use
+# header comments (like the line 'Load packages', above) to keep this section
+# organised.
+
+
+
+# VISUALISE --------------------------------------------------------------------
+# Create your maps, charts, etc. here. If you need to create multiple graphics,
+# use header comments to keep this section organised. Remember to save the
+# graphics you make using `ggsave()`.
